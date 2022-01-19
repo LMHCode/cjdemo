2a4a6351fca34d6a0cc1041a41eda2e56e805bd4
liuminghui <minghui.liu@transsnet.com>
2022-01-19 17:26:33 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,11 @@ var path = require('path')
 var webpack = require('webpack')
 
 module.exports = {
-  entry: './src/main.js',
+  entry: './src/lib/index.js',
   output: {
     path: path.resolve(__dirname, './dist'),
     publicPath: '/dist/',
-    filename: 'build.js'
+    filename: 'vue-pay-pop.js'
   },
   module: {
     rules: [
